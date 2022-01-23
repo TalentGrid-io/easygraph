c9f8874df5df8c0183183468376e557e1f813267
Talha Emin Aydemir <talhaaydemir@teknasyon.com>
2022-01-24 02:37:22 +0300

    add fastify platform

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,10 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
+import {
+    FastifyAdapter,
+    NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    app.useGlobalPipes(new ValidationPipe());
+    const app = await NestFactory.create<NestFastifyApplication>(
+        AppModule,
+        new FastifyAdapter(),
+    );
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            errorHttpStatusCode: 400,
+        }),
+    );
 
     await app.listen(process.env.PORT);
     console.log(`Application is running on: ${await app.getUrl()}`);
