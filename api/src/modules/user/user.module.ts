a6bdccd4e273ce3a69e4493eca6e9c30549566e0
Talha Emin Aydemir <talhaaydemir@teknasyon.com>
2022-01-24 02:43:08 +0300

    add user module exports and providers

--- api/src/modules/user/user.module.ts
+++ api/src/modules/user/user.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
+import { UserService } from './user.service';
 
 @Module({
-    imports: [],
-    providers: [],
+    imports: [TypeOrmModule.forFeature([UserRepository])],
+    providers: [UserService],
+    exports: [UserService],
 })
 export class UserModule {}
