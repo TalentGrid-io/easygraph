a7b34121b8f7146480e382284444f10f522adc3a
Talha Emin Aydemir <talhaaydemir@teknasyon.com>
2022-01-26 00:07:40 +0300

    implement design

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const query = `query Match {
+    match {
+        user {
+            fullname,
+            email
+        },
+        position {
+            name,
+            status
+        },
+        score
+    }
+}`;
+
+class App extends React.Component {
+    state = {
+        graphql_url: 'http://localhost:4010/graphql'
+    }
+
+    connect = () => {}
+
+    generate = () => {}
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>EasyGraph</h1>
+                    <h2>Generate easy graphql queries</h2>
+                </header>
+                <section>
+                    <div className="App-container">
+                        <div className="App-container-item">
+                            <div className="App-container-connection">
+                                <input type="text" value={this.state.graphql_url} />
+                                <button onClick={this.connect}>Connect</button>
+                            </div>
+
+                            <div className="App-container-item-border-box">
+                                <h1>Match Type</h1>
+                                <ul className="App-container-item-border-box-fields">
+                                    <li>
+                                        <input type="checkbox" />
+                                        <span className="field">id:</span> <span className="type">ID</span>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox" />
+                                        <span className="field">user:</span> <span className="type">User</span>
+                                        <ul>
+                                            <li>
+                                                <input type="checkbox" />
+                                                <span className="field">fullname:</span> <span className="type">String</span>
+                                            </li>
+                                            <li>
+                                                <input type="checkbox" />
+                                                <span className="field">email:</span> <span className="type">String</span>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox" />
+                                        <span className="field">position:</span> <span className="type">Position</span>
+                                        <ul>
+                                            <li>
+                                                <input type="checkbox" />
+                                                <span className="field">name:</span> <span className="type">String</span>
+                                            </li>
+                                            <li>
+                                                <input type="checkbox" />
+                                                <span className="field">status:</span> <span className="type">Boolean</span>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox" />
+                                        <span className="field">score:</span> <span className="type">Int</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="App-container-item">
+                            <button className="App-container-item-generate" onClick={this.generate}>Generate</button>
+                        </div>
+                        <div className="App-container-item">
+                            <div className="App-container-item-border-box">
+                                <pre className="App-container-item-query">
+                                    {query}
+                                </pre>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        );
+    }
 }
 
 export default App;
